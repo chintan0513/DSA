8e76fe7728d7e6646f8fe8dac76c44e12b227486
chintan0513 <chintantripathi1310@gmail.com>
2025-09-11 22:46:04 -0400

    Palindrome number

---
+++ Math/Palindrome.py
@@ -0,0 +1,24 @@
+class solution:
+    def isPalindrome(self, x:int) -> bool:
+        if x<0: return False
+        
+        divider = 1
+        while x >= 10 * divider:
+            divider *= 10
+        
+        while x:
+            right = x % 10
+            left = x // divider
+
+            if left != right: return False
+            else:
+
+                x = (x % divider) // 10
+                divider = divider // 100
+        return True
+
+s = solution().isPalindrome(56765)
+print(s)
+
+
+

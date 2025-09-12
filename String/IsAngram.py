76fdd86b23296418556c7dc07fc9ace670a61345
chintan0513 <chintantripathi1310@gmail.com>
2025-09-12 09:28:58 -0400

    string: valid inagram

---
+++ String/IsAngram.py
@@ -0,0 +1,23 @@
+class Solution:
+    def isAnagram(Self, s:str, t:str) -> bool:
+
+        if len(s) != len(t): return False
+
+        countS, countT = {}, {}
+
+        for i in range(len(s)):
+            countS[s[i]] = 1 + countS.get(s[i], 0)
+            countT[t[i]] = 1 + countT.get(t[i], 0)
+        
+        for c in countS:
+            if countS[c] != countT.get(c, 0):
+                return False
+        return True
+
+# Time Complexity: O(n)
+# Space Complexity: O(1)
+
+# test case
+s = "anagram"
+t = "nagaram"
+print(Solution().isAnagram(s, t))  # Output: True

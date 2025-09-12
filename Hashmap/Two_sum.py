a9564e492107a85ff84b8a759ec7a6be9971d9d6
chintan0513 <chintantripathi1310@gmail.com>
2025-09-11 20:35:10 -0400

    Hashmap: Two sum with test cases

--- Hashmap/Two_sum.py
+++ Hashmap/Two_sum.py
@@ -1,3 +1,5 @@
+from typing import List
+
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
         prevMap = {} # store val: index
@@ -10,6 +12,12 @@ class Solution:
                 prevMap[n] = i
         return
 
+twoSum = Solution().twoSum
+# test cases
+print(twoSum([2,7,11,15], 9)) # [0,1]
+print(twoSum([3,2,4], 6)) # [1,2]
+
 # Time: O(n)
 # Space: O(n)
 # Hashmap
+
